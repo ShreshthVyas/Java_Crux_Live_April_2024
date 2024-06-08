a79c964bbd7c573ad6742f2267cb6d5fe294275e
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2024-06-08 22:56:21 +0530

    Update PalindromePartioning.java

--- Java_Crux_Live_2024_April/src/Lecture23/PalindromePartioning.java
+++ Java_Crux_Live_2024_April/src/Lecture23/PalindromePartioning.java
@@ -1,48 +1,38 @@
 package Lecture23;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PalindromePartioning {
-	static List<List<String>> list  = new ArrayList<>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "nitin";
-		palpart(s,new ArrayList<>());
 		
-		System.out.println(list);
+		String ques = "nitin";
+		palpart(ques, "");
 		
 	}
-
-	public static void palpart(String ques,List<String> ll) {
+	public static void palpart(String ques,String ans) {
 		// TODO Auto-generated method stub
-		if(ques.length() == 0) {
-			list.add(ll);
+		if(ques.length()==0) {
+			System.out.println(ans);
 			return;
 		}
-		
-		for (int i = 1; i <= ques.length(); i++) {
+		for (int i = 1; i <=ques.length(); i++) {
 			String s = ques.substring(0,i);
 			if(isPal(s)) {
-				ll.add(s);
-				palpart(ques.substring(i), ll);
+				palpart(ques.substring(i),ans+s+'|');
 			}
+			
 		}
 		
 	}
 	public static boolean isPal(String s) {
 		int i = 0;
 		int j = s.length()-1;
-		
 		while(i<j) {
-			if(s.charAt(i)!=s.charAt(j)) {
+			if(s.charAt(i)!= s.charAt(j)) {
 				return false;
 			}
 			i++;
 			j--;
 		}
-		
 		return true;
 	}
-
 }
